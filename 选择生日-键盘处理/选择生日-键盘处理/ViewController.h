94a8a1f0bfeeaff9ea81181fb632720c12f03d88
susu <28025920@qq.com>
2016-04-14 11:45:43 +0800

    创建项目

---
+++ 选择生日-键盘处理/选择生日-键盘处理/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  选择生日-键盘处理
+//
+//  Created by  江苏 on 16/4/14.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 选择生日-键盘处理/选择生日-键盘处理/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  选择生日-键盘处理
+//
+//  Created by  江苏 on 16/4/14.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
